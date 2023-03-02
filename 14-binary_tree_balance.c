e46a1815eba6e519f6a7e9ceac5f70917e63b9be
Rehema Owino <sayeakinyi@gmail.com>
2023-03-02 00:05:05 -0800

    Balance factor

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ *
+ * Description: Height balanced binary trees can be denoted by HB(k),
+ * where k is the difference between heights of left and right subtrees.
+ * 'k' is known as the balance factor
+ *
+ * Return: balance factor, otherwise 0 if tree is null
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+
+	return (0);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: number of levels in a binary tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t l = 0, r = 0;
+
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
+	}
+	return (0);
+}
